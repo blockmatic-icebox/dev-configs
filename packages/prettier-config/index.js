9929181e121f2d6bf11e79c10995a7af77ec1f00
gaboesquivel <contact@gaboesquivel.com>
2019-12-13 12:20:53 -0600

    feat: jsx single quote

--- packages/prettier-config/index.js
+++ packages/prettier-config/index.js
@@ -2,21 +2,9 @@
 // https://prettier.io/docs/en/options.html
 
 module.exports = {
-  // printWidth: 80,
   tabWidth: 2,
-  // useTabs: false,
   semi: false,
   singleQuote: true,
-  // jsxSingleQuote: false,
+  jsxSingleQuote: true,
   trailingComma: 'all',
-  // bracketSpacing: true,
-  // jsxBracketSameLine: false,
-  // arrowParens: 'avoid',
-  // rangeStart: 0,
-  // rangeEnd: Infinity,
-  // requirePragma: false,
-  // insertPragma: false,
-  // proseWrap: 'preserve',
-  // htmlWhitespaceSensitivity: 'css',
-  // endOfLine: 'auto',
 }
